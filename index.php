1698bb2ddd50f14d202f155fb3ec45a27db2187e
Joemari Sevilla <joemsevilla@gmail.com>
2015-10-10 10:55:47 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<title>Cryptanoia : Miscellany of Cryptography </title>
+<head>
+    <link href="css/master.css" rel="stylesheet" type="text/css"/>
+<style type="text/css"> div.div1 { 
+    width:100%;
+     height:100%;
+    background:url(images/bg1.jpg) no-repeat;
+    position: relative;
+   }
+   div.div1:hover { 
+    width:300px;
+     height:400px;
+     background: #F0E4E6;
+  
+   }
+
+
+div.div2 {  width:100%;
+     height:100%;
+   opacity:0.8; 
+  filter:alpha(opacity=40); 
+}
+g
+{
+color:#000000;
+font-size: 19px;
+
+height: 10px;
+border-top: 10px;
+}
+</style>
+</head>
+<body>
+<div class="container">
+<div id="main-nav">
+  <ul>
+    <li><a href="index.php">Home</a></li>
+   
+    <li><a>Cryptography<span class="drop-down"></span></a>
+      <ul>
+        <li><a href="about+morse.php">Morse Code</a></li>
+        <li><a href="about+otp.php">One Pad Time</a></li>
+        <li><a href="about+atbash.php">Atbash</a></li>
+        <li><a href="about+bacon.php">Baconian Cipher</a></li>
+    
+      </ul>
+    </li>
+     <li><a href ="about.php">About<span class="drop-down"></span></a>
+      
+    <li><a href="contact.php">Contact<span class="drop-down"></span></a>
+     </li>
+  </ul>
+</div>
+<div class="header"><img src= "images/bg1.png" ></div>
+ <div id="Footer1"></div>
+     <div id="Footer">
+
+
+
+<div class="m">Morse Code</div>
+<div class="dev">This website is developed by Joshua Natad  and Joemari Sevilla. 
+
+</div>
+  </div>
+  </div>
+</div>
+</body>
+</html>

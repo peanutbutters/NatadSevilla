876b61ba79422cc88dd10880034dff61f00884b5
Joshua Natad <joshuanatad@gmail.com>
2015-10-10 10:54:45 +0800

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<title>Cryptanoia : Miscellany of Cryptography </title>
+<head>
+    <link href="css/master.css" rel="stylesheet" type="text/css"/>
+<style type="text/css"> 
+
+#boody{
+    background-color:   #CCCCCC ;
+    height: 320px;
+    width: 80%;
+    position: static;  
+    clear:both;
+    margin-bottom: 80px;
+   
+}
+.dev {
+  position: relative;
+  float: left;
+  bottom: -35px;
+  left: 30px;
+  font-family: 'monospace', Arial;
+  color: #FFFFFF;
+  font-size: 17px;
+  opacity: 0.8;
+}
+.devs {
+  position: relative;
+  float: left;
+  bottom: -35px;
+  left: 30px;
+  font-family: 'monospace', Arial;
+  color: #CCCCCC;
+  font-size: 13px;
+  opacity: 0.8;
+}
+</style>
+</head>
+<body>
+<div class="container">
+<div id="main-nav">
+  <ul>
+    <li><a href="index.php">Home</a></li>
+   
+    <li><a>Cryptography<span class="drop-down"></span></a>
+      <ul>
+        <li><a href="about+morse.php">Morse Code</a></li>
+        <li><a href="about+otp.php">One Pad Time</a></li>
+        <li><a href="about+atbash.php">Atbash</a></li>
+        <li><a href="about+bacon.php">Baconian Cipher</a></li>
+    
+      </ul>
+    </li>
+     <li><a href ="about.php">About<span class="drop-down"></span></a>
+      
+    <li><a href="contact.php">Contact<span class="drop-down"></span></a>
+     
+    </li>
+   
+  </ul>
+</div>
+<div id="Heading">
+</div>
+<div id="Header3"><center><div id="title"> Contact</div></font></center><br><br>
+<div id="boody">
+<div class="dev"><pre>
+
+  Contact Details:
+
+  Email: joshuanatad@gmail.com
+  Contact Number: 09352795355
+  Facebook: https://www.facebook.com/jnmopi
+
+  Email: joemarisevilla@gmail.com
+  Contact Number: 09067322651
+  Facebook: https://www.facebook.com/JoemariIgnacioSevilla?ref=ts&fref=ts
+
+</pre>
+</div>
+
+</div>
+     <div id="Footer">
+
+<div class="devs"><br>This website is developed by Joshua Natad  and Joemari Sevilla. <br>
+  Published (2015, October)
+
+
+</div>
+  </div>
+  </div>
+</div>
+</body>
+</html>

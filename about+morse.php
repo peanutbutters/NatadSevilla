3e6175a9b60c01c6888cbd16d829f08a3e01eed3
Joshua Natad <joshuanatad@gmail.com>
2015-10-10 10:46:31 +0800

    Create about+morse.php

---
+++ about+morse.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<title>Morse Code</title>
+<head>
+    <link href="css/menu.css" rel="stylesheet" type="text/css"/>
+
+</head>
+<style>
+
+.icon {
+  position: relative;
+  height: 200px;
+  width: 200px;
+  border-radius: 300px;
+  border-top-left-radius: 300px;
+  border-bottom-right-radius: 300px;
+  border-top-right-radius: 300px;
+  border-bottom-left-radius: 300px;
+  background-color: #C0C0C0;
+  border: 5px solid #EBEBEB;
+  top: 20px;
+  -webkit-transform: translate(-100%, 0);
+     -moz-transform: translate(-200%, 0);
+      -ms-transform: translate(-1s0%, 0);
+       -o-transform: translate(-10%, 0);
+          transform: translate(-10%, 0);
+  -webkit-transition: transform 1s ease-in;
+     -moz-transition: transform 1s ease-in;
+       -o-transition: transform 1s ease-in;
+          transition: transform 1s ease-in;
+
+  
+}
+
+
+
+
+.icon:hover {
+ -webkit-transform: translate(0, 0);
+     -moz-transform: translate(0, 0);
+      -ms-transform: translate(0, 0);
+       -o-transform: translate(0, 0);
+          transform: translate(0, 0);
+        }
+
+</style>
+
+
+<body>
+
+  <div id="Container">
+    <div id="main-nav">
+  <ul>
+    <li><a href="index.php">Home</a></li>
+   
+    <li><a>Cryptography<span class="drop-down"></span></a>
+      <ul>
+        <li><a href="about+morse.php">Morse Code</a></li>
+        <li><a href="about+otp.php">One Pad Time</a></li>
+        <li><a href="about+atbash.php">Atbash</a></li>
+        <li><a href="about+bacon.php">Baconian Cipher</a></li>
+    
+      </ul>
+    </li>
+     <li><a href ="about.php">About<span class="drop-down"></span></a>
+      
+    <li><a href="contact.php">Contact<span class="drop-down"></span></a>
+     
+    </li>
+   
+  </ul>
+</div>
+  <div id="Header"><br><br>
+    <div id="title">Morse Code</div>
+
+  </div>
+  
+  <div id="Header2">
+<table cellpadding=0 cellspacing=0 border=0 width=100%>
+<tr>
+<td valign=top width="99%">
+<div id="fs"><br><pre> 
+
+
+
+
+
+
+".... . .-.. .-.. --- .-- --- .-. .-.. -.."
+
+
+
+You didn't get that didn't you?
+It says <font color="black" size="40">"Hello World"</font>
+
+
+          </pre>
+<br><br>
+<br><br>
+<br><br>
+<br><br></div>
+
+
+  </div>
+
+                    <div id="Header">
+     <center> <div id="fs"><br>  <img src="images/558.jpg" class="icon">   <center></img>
+<br>
+<font size="40" color="white">What is Morse Code?
+   <hr width=20% color=white></font></font>
+<pre> <font color="white">  Morse code is a method of transmitting text information 
+as a series of on-off tones, lights, or clicks that can
+be directly understood by a skilled listener or observer
+without special equipment. The International Morse Code
+encodes the ISO basic Latin alphabet, standardized
+sequences of short and long signals called "dots" and
+"dashes", or "dits" and "dash", as in amateur radio 
+practice. Because many non-English natural languages
+use more than the 26 Roman letters, extensions to the
+Morse alphabet exist for those languages.<br></font>
+</pre>
+
+  </div>
+
+                  </div>
+
+
+
+            <div id="Header2">
+     <div id="fs"><br>
+ <pre>
+<img src="images/Morse.jpg" class="icon"> 
+
+
+  Morse Code is created by Samuel Morse, was
+  designed to transmit letters across telegrams. 
+  He wanted frequently used letters to have short 
+  codes and less frequently used letters to have 
+  longer codes.
+
+
+Would you like to learn more about Morse Code?
+  <br><br></pre>
+<a href="morse.php" class="action-button shadow animate blue">TRY!</a></center>
+
+    </div>
+<center>
+            </div>
+  <div id="Footer1"></div>
+     <div id="Footer">
+
+
+
+<div class="m">Morse Code</div>
+<div class="dev">This website is developed by  Joshua Natad  and Joemari Sevilla. 
+
+</div>
+  </div>
+  </div>
+</body>
+</html>
